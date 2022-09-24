1a7b8d2b253661e809d0e1fe8dff62d32f715143
dav1d992 <daviden@live.dk>
2022-09-24 15:34:30 +0200

    Added article fetch + sidebar logic

--- news-app/src/app/app.component.ts
+++ news-app/src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Article } from './models/article';
+import { ArticleResponseData } from './models/article-response-data';
 import { Source } from './models/source';
+import { SourceResponseData } from './models/source-response-data';
 import { NewsService } from './news/news.service';
 
 @Component({
@@ -9,23 +18,47 @@ import { NewsService } from './news/news.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'news-app';
   sources: Source[] = [];
   articles: Article[] = [];
+  selectedNewsChannel: string = 'Top 10 Trending News!';
 
   @ViewChild(MatSidenav) sideNav!: MatSidenav;
 
-  constructor(private newsService: NewsService) {}
+  constructor(
+    private newsService: NewsService,
+    private observer: BreakpointObserver,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.newsService.getArticles().subscribe((res) => {
-      console.log('articles ', res);
-      this.articles = res;
+    this.newsService.getArticles().subscribe((res: ArticleResponseData) => {
+      this.articles = res.articles;
     });
-    this.newsService.getSources().subscribe((res) => {
-      console.log('sources ', res);
-      this.sources = res;
+    this.newsService.getSources().subscribe((res: SourceResponseData) => {
+      this.sources = res.sources;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)']).subscribe((res) => {
+      if (res?.matches) {
+        this.sideNav.mode = 'over';
+        this.sideNav.close();
+      } else {
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    });
+    this.cd.detectChanges();
+  }
+
+  searchSource(source: any) {
+    this.newsService.getArticlesBySource(source.id).subscribe((res: any) => {
+      this.selectedNewsChannel = source.name;
+      this.articles = res.articles;
     });
   }
 }
